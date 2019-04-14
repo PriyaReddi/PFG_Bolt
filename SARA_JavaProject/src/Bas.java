be397b49fa9a7a4ab6782b223f5cc4a7119ab707
Dell <agautami0503@gmail.com>
2019-04-15 00:29:41 +0530

    Sara

---
+++ SARA_JavaProject/src/Bas.java
@@ -0,0 +1,25 @@
+
+public class Bas  {
+
+	public static void main(String[] args) {
+		StaticAdnNonStatic addb = new StaticAdnNonStatic();
+		
+	}
+	
+public static void m1() {
+		
+		System.out.println("HI............");
+		
+	}
+
+}
+
+
+class longs extends Bas
+{
+    public static void main(String args[])
+           {
+                  long a = 100000L;
+                  System.out.println(a);
+           }
+}
---
+++ SARA_JavaProject/src/StaticAdnNonStatic.java
@@ -0,0 +1,21 @@
+
+public class StaticAdnNonStatic {
+	
+	static int a = 10;
+	
+	public static void main (String args[])
+	{
+	//	StaticAdnNonStatic ab = new StaticAdnNonStatic();
+	
+	System.out.println(a);
+	}
+	
+	public static void m1() {
+		
+		System.out.println("static");
+		
+	}
+	
+	
+}
+
