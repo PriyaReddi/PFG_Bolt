3c402f3e24ef89c36dfc302fc74cf5c186d8ff49
PriyaReddi <49553370+PriyaReddi@users.noreply.github.com>
2019-04-15 12:19:44 +0530

    Update B.java

--- SARA_JavaProject/src/B.java
+++ SARA_JavaProject/src/B.java
@@ -4,7 +4,7 @@ public class B {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("priya baby");
-		System.out.println("saranya baby");
+		//System.out.println("saranya baby");
 
 	}
 
