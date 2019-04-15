50ce4c52e62a037f64f8e12088d5d192668bc804
PriyaReddi <49553370+PriyaReddi@users.noreply.github.com>
2019-04-15 12:32:02 +0530

    Update A.java

--- SARA_JavaProject/src/A.java
+++ SARA_JavaProject/src/A.java
@@ -4,7 +4,7 @@ public class A {
 public static void main (String args[])
 {
 
-System.out.println("Hi bab 5uwk8e");
+//System.out.println("Hi bab 5uwk8e");
 
 System.out.println("Hsd fgvhbhjnkm");
   System.out.println("Hsd fgvhbhjnkm dsfdgfdfg");
