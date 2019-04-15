9f2b44914640af1bf1bae9e07dc39b002890556d
PriyaReddi <49553370+PriyaReddi@users.noreply.github.com>
2019-04-15 11:44:29 +0530

    Update A.java

--- SARA_JavaProject/src/A.java
+++ SARA_JavaProject/src/A.java
@@ -3,7 +3,6 @@ import com.sun.java_cup.internal.runtime.Symbol;
 public class A {
 public static void main (String args[])
 {
-System.out.println("Hi bab dcvfsbmegnayrjr5uwk8e");
-  System.out.println("HifSGzsr htjykguytedfgh);
+System.out.println("Hsd fgvhbhjnkm");
 }
 }
